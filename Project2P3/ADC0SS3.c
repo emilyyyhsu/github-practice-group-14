394ebb66715d0403b031ee54047659ed96520abc
Suphia Sidiqi <suphia.sidiqi01@student.csulb.edu>
2025-10-14 18:05:42 -0700

    Update ADC0SS3.c

--- Project2P3/ADC0SS3.c
+++ Project2P3/ADC0SS3.c
@@ -10,6 +10,13 @@
 // This file provide initialization function for two analog channels:
 // PE2/AIN1 and PD0/AIN7
 
+
+
+
+// this file is going to be a ADC1SS2 module to take care of three sensor outputs
+// example project look at  WallFollower
+// add two more sensors to robot and implement function object_follower in following_robot.c
+// default state is to be in mode 1
 #include "tm4c123gh6pm.h"
 #include <stdint.h>
 
@@ -28,7 +35,62 @@
 // SS3 triggering event: software trigger
 // SS3 1st sample source: Ain7 (PD0)
 // SS3 interrupts: flag set on completion but no interrupt requested
-// When the PLL is operating, the ADC clock is derived from the PLL(400) ÷ 25 by default.
+// When the PLL is operating, the ADC clock is derived from the PLL(400) ï¿½ 25 by default.
+
+// Initializes AIN2, AIN9, and AIN8 sampling
+// 125k max sampling
+// SS2 triggering event: software trigger, busy-wait sampling
+// SS2 1st sample source: AIN2 (PE1)
+// SS2 2nd sample source: AIN9 (PE4)
+// SS2 3rd sample source: AIN8 (PE5)
+// SS2 interrupts: enabled after 3rd sample but not promoted to controller
+void ADC_Init298(void){
+  volatile unsigned long delay;
+//  SYSCTL_RCGC0_R |= 0x00010000; // 1) activate ADC0 (legacy code)
+  SYSCTL_RCGCADC_R |= 0x00000001; // 1) activate ADC0
+  SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R4; // 1) activate clock for Port E
+  delay = SYSCTL_RCGCGPIO_R;      // 2) allow time for clock to stabilize
+  delay = SYSCTL_RCGCGPIO_R;
+  GPIO_PORTE_DIR_R &= ~0x32;      // 3) make PE1, PE4, and PE5 input
+  GPIO_PORTE_AFSEL_R |= 0x32;     // 4) enable alternate function on PE1, PE4, and PE5
+  GPIO_PORTE_DEN_R &= ~0x32;      // 5) disable digital I/O on PE1, PE4, and PE5
+                                  // 5a) configure PE4 as ?? (skip this line because PCTL is for digital only)
+  GPIO_PORTE_PCTL_R = GPIO_PORTE_PCTL_R&0xFF00FF0F;
+  GPIO_PORTE_AMSEL_R |= 0x32;     // 6) enable analog functionality on PE1, PE4, and PE5
+  ADC0_PC_R &= ~0xF;              // 8) clear max sample rate field
+  ADC0_PC_R |= 0x1;               //    configure for 125K samples/sec
+  ADC0_SSPRI_R = 0x3210;          // 9) Sequencer 3 is lowest priority
+  ADC0_ACTSS_R &= ~0x0004;        // 10) disable sample sequencer 2
+  ADC0_EMUX_R &= ~0x0F00;         // 11) seq2 is software trigger
+  ADC0_SSMUX2_R = 0x0892;         // 12) set channels for SS2
+  ADC0_SSCTL2_R = 0x0600;         // 13) no D0 END0 IE0 TS0 D1 END1 IE1 TS1 D2 TS2, yes END2 IE2
+  ADC0_IM_R &= ~0x0004;           // 14) disable SS2 interrupts
+  ADC0_ACTSS_R |= 0x0004;         // 15) enable sample sequencer 2
+}
+
+//------------ADC_In298------------
+// Busy-wait Analog to digital conversion
+// Input: none
+// Output: three 12-bit result of ADC conversions
+// Samples AIN8, AIN9, and AIN2
+// 125k max sampling
+// software trigger, busy-wait sampling
+// data returned by reference
+// ain2 (PE1) 0 to 4095
+// ain9 (PE4) 0 to 4095
+// ain8 (PE5) 0 to 4095
+void ADC_In298(unsigned long *ain2, unsigned long *ain9, unsigned long *ain8){
+  ADC0_PSSI_R = 0x0004;            // 1) initiate SS2
+  while((ADC0_RIS_R&0x04)==0){};   // 2) wait for conversion done
+  *ain2 = ADC0_SSFIFO2_R&0xFFF;    // 3A) read first result
+  *ain9 = ADC0_SSFIFO2_R&0xFFF;    // 3B) read second result
+  *ain8 = ADC0_SSFIFO2_R&0xFFF;    // 3C) read third result
+  ADC0_ISC_R = 0x0004;             // 4) acknowledge completion
+}
+
+
+
+/*
 void ADC0_InitSWTriggerSeq3_Ch7(void){ 
   SYSCTL_RCGCGPIO_R |= SYSCTL_RCGCGPIO_R3;   // 1) activate clock for Port D
 	while ((SYSCTL_RCGCGPIO_R&SYSCTL_RCGCGPIO_R3)!=SYSCTL_RCGCGPIO_R3){}
@@ -93,3 +155,4 @@ uint16_t ADC0_InSeq3(void){
   ADC0_ISC_R = ADC0_ISC_SS3;             // 4) acknowledge completion
   return result;
 }
+*/

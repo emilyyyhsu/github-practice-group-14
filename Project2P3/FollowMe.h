c68159727fa0a11b37af7b71e94257e6fffd2dc0
AnaArante01 <AnaJolynn.Arante01@student.csulb.edu>
2025-10-16 16:09:16 -0700

    Added FollowMe.h

--- Project2P3/FollowMe.h
+++ Project2P3/FollowMe.h
@@ -4,26 +4,19 @@
 // Description: 
 /////////////////////////////////////////////////////////////////////////////
 
-void Timer1A_Delay(unsigned long period);
+#include "Motors.h" 
 
-#define ONE_SEC		64000
-
-#include "tm4c123gh6pm.h"
-
-void stop_the_car(void){
-		WHEEL_DIR = STOP;
-		PWM0_ENABLE_R &= ~0x0000000C; // stop both wheels
-		Timer1A_Delay(ONE_SEC);
+void 	move_forward(void){
+	WHEEL_DIR = FORWARD;					// Enable left and right motors
+	PWM0_ENABLE_R |= 0x0000000C;	// Enable PB54:PWM1 output 2&3
 }
 
-void move_backward(void){
-		WHEEL_DIR = BACKWARD;
-		PWM0_ENABLE_R |= 0x0000000C; // enable both wheels
-		Timer1A_Delay(ONE_SEC);
+void 	move_backward(void){
+	WHEEL_DIR = BACKWARD;					// Enable
+	PWM0_ENABLE_R |= 0x0000000C;	// Enable PB54:PWM1 output 2&3
 }
-	
-void move_forward(void){
-		WHEEL_DIR = FORWARD;
-		PWM0_ENABLE_R |= 0x0000000C; // enable both wheels
-		Timer1A_Delay(ONE_SEC);
+
+void 	stop_the_car(void){
+	WHEEL_DIR = STOP;							// Disable left and right motors
+	PWM0_ENABLE_R &= ~0x0000000C;	// Enable PB54:PWM1 output 2&3
 }
